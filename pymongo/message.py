f7aad6907bd9cca692244ae671dfc8b4eb2c9d0e
Prashant Mital <prashantmital@gmail.com>
2019-04-09 15:08:08 -0700

    don't skip user fields when provided in OpReply

--- pymongo/message.py
+++ pymongo/message.py
@@ -1416,6 +1416,9 @@ class _OpReply(object):
             :class:`~bson.codec_options.CodecOptions`
         """
         self.raw_response(cursor_id)
+        if user_fields:
+            return bson._decode_all_selective(
+                self.documents, codec_options, user_fields)
         if legacy_response:
             return bson.decode_all(self.documents, codec_options)
         return bson.decode_all(
